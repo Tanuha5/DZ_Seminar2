705dd464064a3880d49c437f4ab3446ae701d4c8
Tatiana <Tanuha5@rambler.ru>
2023-02-13 14:14:31 +0300

    Рабочий или выходной день недели

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает 
+// на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+System.Console.Write("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if(day == 1 || day == 2 || day == 3 || day == 4 || day == 5)
+{
+System.Console.WriteLine("Рабочий");
+}
+else if(day == 6 || day == 7)
+{
+System.Console.WriteLine("Выходной");
+}
+else
+{
+System.Console.WriteLine("Такого дня недели не существует");
+}
